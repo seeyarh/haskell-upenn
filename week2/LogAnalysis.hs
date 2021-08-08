80e89630ac2d1b7e59bf43b4aa6a3959a156918f
Collins Huff <collinsrhuff@gmail.com>
2021-08-08 17:50:27 -0400

    week 2, exercises 2-6

--- week2/LogAnalysis.hs
+++ week2/LogAnalysis.hs
@@ -42,3 +42,56 @@ parseTimestamp tokens = if null tokens then Nothing
                         else case readMaybe (head tokens) of
                             Nothing -> Nothing
                             Just ts -> Just (ts, drop 1 tokens)
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert logMessage tree = case logMessage of
+                         Unknown _ -> tree
+                         LogMessage _ ts _ -> do
+                            case tree of
+                                Leaf -> Node Leaf logMessage Leaf
+                                Node left treeLogMessage right -> do
+                                    case treeLogMessage of
+                                        Unknown _ -> tree
+                                        LogMessage treeMessageType treeTs treeMessage -> do
+                                            if treeTs > ts then do
+                                                let newLeft = insert logMessage left
+                                                Node newLeft (LogMessage treeMessageType treeTs treeMessage) right
+                                            else do
+                                                let newRight = insert logMessage right
+                                                Node left (LogMessage treeMessageType treeTs treeMessage) newRight
+
+build :: [LogMessage] -> MessageTree
+build logMessages = do
+    let tree = Leaf
+    buildHelper logMessages tree
+
+buildHelper :: [LogMessage] -> MessageTree -> MessageTree
+
+buildHelper logMessages tree = if null logMessages then tree
+                               else do
+                                let newTree = insert (head logMessages) tree
+                                buildHelper (drop 1 logMessages) newTree
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder tree = case tree of
+                    Leaf -> []
+                    Node left logMessage right -> inOrder left ++ [logMessage] ++ inOrder right
+
+isBad :: LogMessage -> Int -> Bool
+isBad logMessage threshold = case logMessage of
+                Unknown _ -> False
+                LogMessage messageType _ _ -> case messageType of
+                    Error severity -> severity > threshold
+                    _ -> False
+
+getMessage :: LogMessage -> String
+getMessage logMessage = case logMessage of
+                 Unknown message -> message
+                 LogMessage _ _ message -> message
+
+
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong logMessages = do
+    let sortedBadLogs = inOrder(build([logMessage | logMessage <- logMessages, isBad logMessage 50]))
+    [getMessage logMessage | logMessage <- sortedBadLogs]

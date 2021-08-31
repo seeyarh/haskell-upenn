244fb4ec15389dd4f6bbbb47555f5aff67d215d4
Collins Huff <collinsrhuff@gmail.com>
2021-08-30 23:20:55 -0400

    week8 hw started

---
+++ week8/Employee.hs
@@ -0,0 +1,81 @@
+module Employee where
+
+import Data.Semigroup
+import Data.Tree
+
+-- Employee names are represented by Strings.
+type Name = String
+
+-- The amount of fun an employee would have at the party, represented
+-- by an Integer
+type Fun = Integer
+
+-- An Employee consists of a name and a fun score.
+data Employee =
+  Emp
+    { empName :: Name
+    , empFun :: Fun
+    }
+  deriving (Show, Read, Eq)
+
+-- A small company hierarchy to use for testing purposes.
+testCompany :: Tree Employee
+testCompany =
+  Node
+    (Emp "Stan" 9)
+    [ Node
+        (Emp "Bob" 2)
+        [ Node (Emp "Joe" 5) [Node (Emp "John" 1) [], Node (Emp "Sue" 5) []]
+        , Node (Emp "Fred" 3) []
+        ]
+    , Node (Emp "Sarah" 17) [Node (Emp "Sam" 4) []]
+    ]
+
+testCompany2 :: Tree Employee
+testCompany2 =
+  Node
+    (Emp "Stan" 9)
+    [ Node
+        (Emp "Bob" 3) -- (8, 8)
+        [ Node
+            (Emp "Joe" 5) -- (5, 6)
+            [ Node (Emp "John" 1) [] -- (1, 0)
+            , Node (Emp "Sue" 5) [] -- (5, 0)
+            ]
+        , Node (Emp "Fred" 3) [] -- (3, 0)
+        ]
+    , Node
+        (Emp "Sarah" 17) -- (17, 4)
+        [ Node (Emp "Sam" 4) [] -- (4, 0)
+        ]
+    ]
+
+-- A type to store a list of guests and their total fun score.
+data GuestList =
+  GL [Employee] Fun
+  deriving (Show, Eq)
+
+instance Ord GuestList where
+  compare (GL _ f1) (GL _ f2) = compare f1 f2
+
+glCons :: Employee -> GuestList -> GuestList
+glCons e@(Emp _ eFun) (GL gl fun) = GL (gl ++ [e]) (fun + eFun)
+
+instance Monoid GuestList where
+  mempty = GL [] 0
+  mappend (GL gl1 fun1) (GL gl2 fun2) = GL (gl1 ++ gl2) (fun1 + fun2)
+
+instance Semigroup GuestList where
+  (<>) = mappend
+
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun gl1@(GL _ f1) gl2@(GL _ f2)
+  | f1 > f2 = gl1
+  | otherwise = gl2
+
+treeFold :: (a -> b -> b) -> b -> Tree a -> b
+treeFold f acc (Node val []) = f val acc
+treeFold f acc (Node val children) = f c val ( 
+  where
+    newAcc = f val acc
+    (c:cs) = map (treeFold f acc) children
